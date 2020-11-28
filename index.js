0305932112f02af8c0eb53f53ef2264e82955aab
DVK-max <70217089+brainiac295@users.noreply.github.com>
2020-11-28 19:16:02 +0530

    Revert "Firebase Added and Locked"
    This reverts commit c74aa3647f8d1a0969653f10b158dd5889368e57.

--- index.js
+++ index.js
@@ -3,31 +3,10 @@ const path=require('path');
 const PORT=process.env.PORT || 5000;
 var app=express();
 
-var admin = require("firebase-admin");
-var serviceAccount = require("./mykey.json");
-const { SIGABRT } = require('constants');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://casuistrynews.firebaseio.com"
-});
-
 i=1;
 
 express()
 .get('/', (req,res)=>{
-    
-    var db=admin.database();
-
-    var Signals=db.ref("Signals");
-
-    var NewSignal=Signals.push();
-
-    NewSignal.set({
-        Xid:NewSignal.key,
-        Number:i
-    });
-
     i++;
     res.send("Hello World "+i);
 }).listen(PORT,()=>console.log('Listening On ${PORT}'))
