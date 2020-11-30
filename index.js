577ade6198d0992ff2c5ad9affc469e5abb0c4ef
DVK-max <70217089+brainiac295@users.noreply.github.com>
2020-11-30 19:13:39 +0530

    Updated for Stocks

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT=process.env.PORT || 5000;
 var app=express();
 //var Upstox=require('upstox');
 
+var url=require('url');
 var admin = require("firebase-admin");
 var serviceAccount = require("./mykey.json");
 const { SIGABRT } = require('constants');
@@ -47,28 +48,92 @@ upstox.connectSocket()
     upstox.on("orderUpdate",function(message)
     {
       var db=admin.database();
-
-    var Signals=db.ref();
-    var NewRoot=Signals.child("UpstoxOrderUpdate");
-    
-    var NewChild=NewRoot.push();
-    NewChild.set(message);      
-    });
-    upstox.on("liveFeed",function(message){
-      var db=admin.database();
-
       var Signals=db.ref();
-      var NewRoot=Signals.child("liveFeed");
-      
+      var NewRoot=Signals.child("UpstoxOrderUpdate");
+    
       var NewChild=NewRoot.push();
-      NewChild.set(message);  
+      NewChild.set(message);
+      if (message.status=="completed" || message.status=="complete")
+      {
+        active=true;
+        price=message.average_price;
+        if (message.transaction_type=="B")
+        {
+          bs=1;          
+          stoploss=price-50;
+          target=price+20;                    
+        } else if (message.transaction_type=="S")
+        {
+          bs=0;
+          stoploss=price+50;
+          target=price-20;
+        }
+      }      
+    });
+
+    upstox.on("liveFeed",function(message){
+      indexBarCount++;
+      if (indexBarCount >=250)
+      {
+            var db=admin.database();
+            var Signals=db.ref();
+            var NewRoot=Signals.child("liveFeed");
+      
+            var NewChild=NewRoot.push();
+            NewChild.set(message);
+      } 
+
+      if (bs==1)
+      {
+        if (message.ltp > targetPrice)
+        {
+          stoploss+=20;
+          targetPrice+=20;
+        }
+
+        if (message.ltp <=stoploss)
+        {
+          upstox.cancelAllOrder({})
+          .then(function(response){})
+          .catch(function(error){});
+        }
+      }
+
+      if (bs==0)
+      {
+        if (message.ltp < targetPrice)
+        {
+          stoploss-=20;
+          targetPrice-=20;
+        }
+
+        if (message.ltp >= stoploss)
+        {
+          upstox.cancelAllOrder({})
+          .then(function(response){})
+          .catch(function(error){});
+        }
+      }      
     });
   });
+
+  var indexBarCount=0;
+
+  var active=false;
+  var stoploss=0;
+  var price=0;
+  var bs=-1;
+  var target=20;
+  var targetPrice=0;
 */
+
 express()
-.get('/', (req,res)=>{
-    
+.get('/', (req,res)=>{    
     var db=admin.database();
+    var q=url.parse(req.url,true).query;
+    var Sym=q.Sym;
+    var Pri=parseFloat(q.Pri);
+    var ST=q.ST;    
 
     var Signals=db.ref("Signals");
 
@@ -76,11 +141,11 @@ express()
 
     NewSignal.set({
         Xid:NewSignal.key,
-        Price:29500,
-        Symbol:"BANKNIFTY20DECFUT",
-        SignalType:"BUY"
+        Price:Pri,
+        Symbol:Sym,
+        SignalType:ST
     });
 
     i++;
-    res.send("Hello World Function Called "+i);
+    res.send("Function Called. Processed.");
 }).listen(PORT,()=>console.log('Listening On ${PORT}'))
